1b91148c854063e0772c4b49c1629b059f1a4bdb
Samirah-M <143946932+Samirah-M@users.noreply.github.com>
2025-03-05 20:46:31 +0000

    postroutes added

---
+++ backend/routes/postRoutes.js
@@ -0,0 +1,12 @@
+// server/routes/postRoutes.js
+const express = require('express');
+const router = express.Router();
+const { createPost, getAllPosts, getPostsByCategory, updatePost, deletePost } = require('../controllers/postController');
+
+router.post('/posts', createPost);
+router.get('/posts', getAllPosts);
+router.get('/posts/category/:category', getPostsByCategory);
+router.put('/posts', updatePost);
+router.delete('/posts/:id', deletePost);
+
+module.exports = router;
