7719fd66f2ae19624d20b78dd4d5e0d1c0f06d93
Samirah-M <143946932+Samirah-M@users.noreply.github.com>
2025-03-05 20:48:14 +0000

    server added

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+// server/server.js
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const postRoutes = require('./routes/postRoutes');
+
+const app = express();
+const port = process.env.PORT || 3003;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api', postRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
